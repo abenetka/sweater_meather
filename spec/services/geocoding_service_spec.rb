a0b4d8ea409eb9657a14cdf8d9c6458a44b450ac
abenetka <ali.benetka@gmail.com>
2019-02-23 13:02:40 -0700

    Adds tests GeocodingService

---
+++ spec/services/geocoding_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe GeocodingService do
+  it 'exists' do
+    location = create(:location, city: "Denver", state: "CO")
+    service = GeocodingService.new(location)
+
+    expect(service).to be_a(GeocodingService)
+  end
+
+  it 'returns latitude and longitude of location' do
+    location = create(:location, city: "Denver", state: "CO")
+    latitude = "39.7392358"
+    longitude = "-104.990251"
+
+    service = GeocodingService.new(location)
+    lat = service.get_latitude
+    long = service.get_longitude
+
+    expect(lat).to eq(latitude.to_f)
+    expect(long).to eq(longitude.to_f)
+  end
+
+end
