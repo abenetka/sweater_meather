691d0290a28af323be23cf611e9dc0eab55ec0db
abenetka <ali.benetka@gmail.com>
2019-02-23 11:39:28 -0700

    Updates location test

--- spec/requests/api/v1/location_request_spec.rb
+++ spec/requests/api/v1/location_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Location API" do
   it 'sends city and state' do
-  location = create(:location)
+  location = create(:location, city: "Denver", state: "CO")
 
-  get "/api/v1/forecast?location=#{location}"
+  get "/api/v1/forecast?location=#{location["city"]}, #{location["state"]}"
 
   expect(response).to be_successful
 
