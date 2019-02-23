b4f12534f09336340937ecc2b71f6554a077555b
abenetka <ali.benetka@gmail.com>
2019-02-23 15:15:34 -0700

    Updates railshelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+require 'webmock/rspec'
 
 SimpleCov.start 'rails' do
   add_filter "app/channels/application_cable/channel.rb"
@@ -19,6 +21,15 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.configure_rspec_metadata!
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<darksky>') { ENV['darksky_api_secret']}
+  config.filter_sensitive_data('<geocoding>') { ENV['google_maps_api_key']}
 end
 # Add additional requires below this line. Rails is not loaded until this point!
 
