a92981696b0b18b23062a8c13dad4026d8dbe487
abenetka <ali.benetka@gmail.com>
2019-02-24 14:53:17 -0700

    Updates Forecast PORO for refactors of geocodeservice, adds new instancer variables

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,40 +1,54 @@
 class Forecast
-  attr_reader :city, :state, :latitude, :longitude
+  attr_reader :latitude, :longitude, :current_weather, :daily_weather, :hourly_weather, :city, :state
   def initialize(location)
     @location = location
     @city = split_location[0]
     @state = split_location[1]
     @latitude = find_latitude
     @longitude = find_longitude
+    @current_weather = get_current_weather
+    @hourly_weather = get_hourly_weather
+    @daily_weather = get_daily_weather
+  end
+
+  def find_longitude
+    location_service.get_coordinates[:lng]
+  end
+
+  def find_latitude
+    location_service.get_coordinates[:lat]
+  end
+
+
+  def get_hourly_weather
+    data = weather_service[:hourly][:data]
+    data.map do |raw_hour|
+      HourlyWeather.new(raw_hour)
+    end
+  end
+
+  def get_daily_weather
+    data = weather_service[:daily][:data]
+    data.map do |raw_day|
+      DailyWeather.new(raw_day)
+    end
+  end
+
+  def get_current_weather
+    data = weather_service[:currently]
+    CurrentWeather.new(data)
   end
 
   def split_location
     @location.split(', ')
   end
 
-  def find_longitude
-    GeocodingService.new(@city, @state).get_longitude
-  end
-
-  def find_latitude
-    GeocodingService.new(@city, @state).get_latitude
-  end
-
-  def get_current_weather
-    weather_service[:currently]
-  end
-
-  def get_hourly_weather
-    weather_service[:hourly]
-  end
-
-  def get_daily_weather
-    weather_service[:daily]
+  def location_service
+    GeocodingService.new(@city, @state)
   end
 
   def weather_service
     DarkSkyService.new.get_weather(@latitude, @longitude)
   end
-
 
 end
