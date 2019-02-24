bcece277d553bb6b7420ad89003c9b241ff6012a
abenetka <ali.benetka@gmail.com>
2019-02-23 22:18:53 -0700

    Adds forecast serializer

---
+++ app/serializers/forecast_serializer.rb
@@ -0,0 +1,17 @@
+class ForecastSerializer
+  include FastJsonapi::ObjectSerializer
+  set_id :object_id
+  attributes :latitude, :longitude, :city, :state
+
+  attribute :current_weather do |object|
+    object.get_current_weather
+  end
+
+  attribute :hourly_weather do |object|
+    object.get_hourly_weather
+  end
+
+  attribute :daily_weather do |object|
+    object.get_daily_weather
+  end
+end
