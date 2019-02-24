71792ee9b594b003d81d6e57385a6621553c38f7
abenetka <ali.benetka@gmail.com>
2019-02-24 14:52:41 -0700

    Updates rails_helper for simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,8 @@ SimpleCov.start 'rails' do
   add_filter "app/jobs/application_job.rb"
   add_filter "app/mailers/application_mailer.rb"
   add_filter "app/helpers/application_helper.rb"
-end
+  add_filter "app/models/application_record.rb"
+end 
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
