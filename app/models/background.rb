f9ff50428d1eaa931f8c833e6c7b4fed716e412c
abenetka <ali.benetka@gmail.com>
2019-02-24 17:35:49 -0700

    Updates background poro

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,23 +1,41 @@
 class Background
+  attr_reader :farm,
+              :server,
+              :id,
+              :secret,
+              :title,
+              :background_url
 
-  def initialize(location)
-    @location = location
-    @city = split_location[0]
-    @state = split_location[1]
-    @latitude = find_latitude
-    @longitude = find_longitude
+  def initialize(attributes)
+    @farm = attributes[:farm]
+    @server = attributes[:server]
+    @id = attributes[:id]
+    @secret = attributes[:secret]
+    @title = attributes[:title]
     @background_url = formatted_background_url
   end
 
-  def get_background_url
-    data = flickr_service[:photos][:photo][0]
-    Flickr.new(data)
-  end
-
   def formatted_background_url
-    results = get_background_url
-    "https://farm#{results.farm}}.staticflickr.com/#{results.server}/#{results.id}_#{results.secret}.jpg"
+    "https://farm#{@farm}.staticflickr.com/#{@server}/#{@id}_#{@secret}.jpg"
   end
+  #
+  # def initialize(location)
+  #   @location = location
+  #   @city = split_location[0]
+  #   @state = split_location[1]
+  #   @latitude = find_latitude
+  #   @longitude = find_longitude
+  #   @background_url = formatted_background_url
+  # end
+
+  # def get_background_url
+  #   data = flickr_service[:photos][:photo][0]
+  #   Flickr.new(data)
+  # end
+
+  # def formatted_background_url
+  #   "https://farm#{results.farm}}.staticflickr.com/#{results.server}/#{results.id}_#{results.secret}.jpg"
+  # end
 
   # def find_longitude
   #   location_service.get_coordinates[:lng]
@@ -30,13 +48,13 @@ class Background
   # def split_location
   #   @location.split(', ')
   # end
-
-  def flickr_service
-    @_flickr_service ||= FlickrService.new.get_photo_json(@latitude, @longitude)
-  end
-
-  def location_service
-     @_location_service ||= GeocodingService.new(@city, @state)
-  end
+  #
+  # def flickr_service
+  #   @_flickr_service ||= FlickrService.new.get_photo_json(@latitude, @longitude)
+  # end
+  #
+  # def location_service
+  #    @_location_service ||= GeocodingService.new(@city, @state)
+  # end
 
 end
