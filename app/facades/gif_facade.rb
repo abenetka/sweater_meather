e36634d144a4e51f8be228c4aabc5f7e21a01fca
abenetka <ali.benetka@gmail.com>
2019-02-25 10:25:59 -0700

    Removes commented-out code

--- app/facades/gif_facade.rb
+++ app/facades/gif_facade.rb
@@ -7,22 +7,6 @@ class GifFacade
     @daily_weather[:data].map do |raw_day|
       Gif.new(raw_day)
     end
-    # giphy_service[:data].map do |raw_gif|
-    #   Gif.new(raw_gif)
-    # end
   end
-  #
-  # def summary
-  #    summary = @daily_weather[:data].map do |day|
-  #     day[:summary]
-  #   end
-  #   binding.pry
-  #   summary.gsub('', '+')
-  # end
-  #
-  # def giphy_service
-  #   @_giphy_service ||= GiphyService.new.get_giphy_json(summary)
-  # end
-
-
+  
 end
