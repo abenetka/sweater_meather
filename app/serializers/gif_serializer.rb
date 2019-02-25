e4cf7793abacc2e1c40ebac2739ea7cc3daffe8a
abenetka <ali.benetka@gmail.com>
2019-02-25 10:47:14 -0700

    Removes commented-out code in GifSerializer

--- app/serializers/gif_serializer.rb
+++ app/serializers/gif_serializer.rb
@@ -1,12 +1,6 @@
 class GifSerializer
   include FastJsonapi::ObjectSerializer
   set_id :object_id
-  # attributes :time, :summary, :url
-
-  attribute :images do |obj|
-    obj.time
-    obj.summary
-    obj.url
-  end
+  attributes :time, :summary, :url
 
 end
