180703785d471a0955969d8cb94111e5a961c6f3
abenetka <ali.benetka@gmail.com>
2019-02-25 10:02:13 -0700

    Updates init in GIF, adds get_giphy, giphy_summary methods

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,46 +1,27 @@
 class Gif
 
   def initialize(daily_weather)
-    binding.pry
-
+    @time = daily_weather[:time]
+    @summary = giphy_summary(daily_weather[:summary])
+    @url = get_giphy_url
   end
-  
+
   def get_giphy
     giphy_service[:data].map do |raw_gif|
         Gif.new(raw_gif)
     end
   end
 
-  # def summary
-  #      summary = @daily_weather[:data].map do |day|
-  #       day[:summary]
-  #   end
-  #     summary.gsub('', '+')
-  #   end
+  def giphy_summary(summary)
+    summary.gsub(" ","+")
+  end
 
-    def giphy_service
-      @_giphy_service ||= GiphyService.new.get_giphy_json(summary)
-    end
+  def giphy_service
+    @_giphy_service ||= GiphyService.new.get_giphy_json(@summary)
+  end
 
-  # attr_reader :farm,
-  #             :server,
-  #             :id,
-  #             :secret,
-  #             :title,
-  #             :background_url
-  #
-  # def initialize(attributes)
-  #   @farm = attributes[:farm]
-  #   @server = attributes[:server]
-  #   @id = attributes[:id]
-  #   @secret = attributes[:secret]
-  #   @title = attributes[:title]
-  #   @background_url = formatted_background_url
-  # end
-  #
-  # def formatted_background_url
-  #   "https://farm#{@farm}.staticflickr.com/#{@server}/#{@id}_#{@secret}.jpg"
-  # end
-
+  def get_giphy_url
+    giphy_service
+  end
 
 end
