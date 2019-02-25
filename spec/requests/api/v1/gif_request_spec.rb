b35d2f8713db47cbdfb07bf25d0fd481b1fde4e6
abenetka <ali.benetka@gmail.com>
2019-02-25 09:37:07 -0700

    Fixes get request path

--- spec/requests/api/v1/gif_request_spec.rb
+++ spec/requests/api/v1/gif_request_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 describe "Gifs API" do
-  it 'returns photo based on weather', :vcr do
+  it 'returns daily giphy based on weather', :vcr do
     location = "Denver, CO"
 
-    get "/api/v1/gifs??location=#{location}"
+    get "/api/v1/gifs?location=#{location}"
 
     expect(response).to be_successful
 
     result = JSON.parse(response.body, symbolize_names: true)
-
     expect(result[:data]).to have_key(:images)
     expect(result[:data][:attributes]).to have_key(:time)
     expect(result[:data][:attributes]).to have_key(:summary)
