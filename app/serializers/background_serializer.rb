41966243dbc891a5dde2e89b4b5fd2be5475e312
abenetka <ali.benetka@gmail.com>
2019-02-24 17:35:27 -0700

    Updates background serializer

--- app/serializers/background_serializer.rb
+++ app/serializers/background_serializer.rb
@@ -1,17 +1,6 @@
 class BackgroundSerializer
   include FastJsonapi::ObjectSerializer
   set_id :object_id
-  attributes :latitude, :longitude, :city, :state
-  #
-  attribute :current_weather do |object|
-    object.get_current_weather
-  end
-  #
-  # attribute :hourly_weather do |object|
-  #   object.get_hourly_weather
-  # end
-  #
-  # attribute :daily_weather do |object|
-  #   object.get_daily_weather
-  # end
+
+  attributes :title, :background_url
 end
