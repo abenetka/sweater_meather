eadcdf7bedde1f117d7b72431b18bd23e7c6b0da
abenetka <ali.benetka@gmail.com>
2019-02-25 11:43:47 -0700

    More attempts!

--- app/serializers/gif_serializer.rb
+++ app/serializers/gif_serializer.rb
@@ -1,27 +1,15 @@
 class GifSerializer
   include FastJsonapi::ObjectSerializer
   set_id :object_id
-# class GifSerializer < ActiveModel::Serializer
-  attributes :url, :summary, :time
-  # attributes :images
-  #
-  # def images
-  #   binding.pry
-  #   self.object do|obj|
-  #     binding.pry
-  #     # {time: obj[:time],
-  #     #   summary: obj[:summary],
-  #     #   url: obj.get_giphy_url
-  #     # }
-  #   end
-  # end
+  attributes images: {:url, :summary, :time, :copyright}
 
   attribute :images do |object|
     binding.pry
     {
       time: object[:time],
       summary: object[:summary],
-      url: object.get_giphy_url
+      url: object.get_giphy_url,
+
     }
   end
 
