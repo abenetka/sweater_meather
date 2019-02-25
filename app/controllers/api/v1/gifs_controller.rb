e5f14c34f93476e10b43d2e52e16d042d8d7aa0a
abenetka <ali.benetka@gmail.com>
2019-02-25 11:42:10 -0700

    Removes commented-out code

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::GifsController < ApplicationController
   def index
     forecast = ForecastFacade.new(params[:location]).get_forecast
     daily_weather = forecast.daily_weather
-    # giphy = Gif.new(daily_weather).get_giphy
     giphy = GifFacade.new(daily_weather).get_giphy
     render json: GifSerializer.new(giphy)
   end
