fbfa8adb207344ec68f05e6b325056a5b11d5077
abenetka <ali.benetka@gmail.com>
2019-02-24 18:42:02 -0700

    Changes Geocoding Service Spec to not have an initialize

--- spec/services/geocoding_service_spec.rb
+++ spec/services/geocoding_service_spec.rb
@@ -4,7 +4,7 @@ describe GeocodingService do
   it 'exists', :vcr do
     city = "Denver"
     state = "CO"
-    service = GeocodingService.new(city, state)
+    service = GeocodingService.new
 
     expect(service).to be_a(GeocodingService)
   end
@@ -12,12 +12,12 @@ describe GeocodingService do
   it 'returns latitude and longitude of location', :vcr do
     city = "Denver"
     state = "CO"
-    latitude = "39.7392358"
-    longitude = "-104.990251"
+    latitude = "39.73924969999999"
+    longitude = "-104.9902505"
 
-    service = GeocodingService.new(city, state)
-    lat = service.get_coordinates[:lat]
-    long = service.get_coordinates[:lng]
+    coordinates = GeocodingService.new.get_coordinates(latitude, longitude)
+    lat = coordinates[:lat]
+    long = coordinates[:lng]
 
     expect(lat).to eq(latitude.to_f)
     expect(long).to eq(longitude.to_f)
