ec8ed45f9099adf23b9f11eb29f4cddef7721eb8
abenetka <ali.benetka@gmail.com>
2019-02-25 09:08:27 -0700

    Adds gif request spec

---
+++ spec/requests/api/v1/gif_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Gifs API" do
+  it 'returns photo based on weather', :vcr do
+    location = "Denver, CO"
+
+    get "/api/v1/gifs??location=#{location}"
+
+    expect(response).to be_successful
+g
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result[:data]).to have_key(:images)
+    expect(result[:data][:attributes]).to have_key(:time)
+    expect(result[:data][:attributes]).to have_key(:summary)
+    expect(result[:data][:attributes]).to have_key(:url)
+  end
+end
