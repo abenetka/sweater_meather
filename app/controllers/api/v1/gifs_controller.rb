f118ebc599d1cb285b1415b72a8e2d8799c97a27
abenetka <ali.benetka@gmail.com>
2019-02-25 09:37:35 -0700

    Updates GifController

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    giphy = Gif.new(params[:location])
+    forecast = ForecastFacade.new(params[:location]).get_forecast
+    daily_weather = forecast.daily_weather
+    giphy = Gif.new(daily_weather)
     render json: GifSerializer.new(giphy)
   end
 end
