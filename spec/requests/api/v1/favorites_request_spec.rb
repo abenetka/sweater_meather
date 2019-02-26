640bb60e997dd6fa8ac3dbe464a4345f22414037
abenetka <ali.benetka@gmail.com>
2019-02-26 14:33:50 -0700

    Adds test for favorite list

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -15,7 +15,7 @@ describe "Favorites API" do
      expect(response).to be_successful
    end
 
-   it 'doesnt posts city and api_key', :vcr do
+   it 'doesnt post city and api_key', :vcr do
      email = "wanda@aol.com"
      password = "12345"
      post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
@@ -29,5 +29,31 @@ describe "Favorites API" do
      expect(response).to_not be_successful
      expect(status).to eq(401)
    end
+
+   it 'gets a list of favorite location', :vcr do
+     email = "wanda@aol.com"
+     password = "12345"
+     post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
+     result = JSON.parse(response.body, symbolize_names: true)[:data]
+
+     api_key = result[:attributes][:api_key]
+     location = "Denver, CO"
+     post "/api/v1/favorites?location=#{location}&api_key=#{api_key}"
+     location_1 = "Denver, CO"
+     location_2 = "Chicago, IL"
+     post "/api/v1/favorites?location=#{location_1}&api_key=#{api_key}"
+     post "/api/v1/favorites?location=#{location_2}&api_key=#{api_key}"
+
+     get "/api/v1/favorites?api_key=#{api_key}"
+     result = JSON.parse(response.body, symbolize_names: true)
+     binding.pry
+
+     expect(response).to be_successful
+     expect(status).to eq(200)
+     expect(result).to have_key(:location)
+     expect(result).to have_key(:current_weather)
+   end
+
+
 
 end
