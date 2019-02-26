2cb01a414be2be015c398b36315d30dad984f177
abenetka <ali.benetka@gmail.com>
2019-02-26 15:01:49 -0700

    Removes commented out code

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -12,11 +12,5 @@ class Api::V1::FavoritesController < ApplicationController
     user = User.find_by(api_key: params[:api_key])
     render json: FavoriteSerializer.new(user)
   end
-  # def index
-  #   user = User.find_by(api_key: params[:api_key])
-  #   binding.pry
-  #   favorites = user.favorites
-  #   favorite = FavoriteFacade.new(user.favorites).fav_current_weather
-  #   render json: FavoriteSerializer.new(favorite)
-  # end
+
 end
