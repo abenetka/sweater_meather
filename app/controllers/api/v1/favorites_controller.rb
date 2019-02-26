01d61253f3a372fcd9b19579dbbc9b7332817550
abenetka <ali.benetka@gmail.com>
2019-02-26 14:35:54 -0700

    Adds WIP for favoritescontroller index

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -6,6 +6,17 @@ class Api::V1::FavoritesController < ApplicationController
     else
       render json: {}, status: :unauthorized
     end
-
   end
+
+  def index
+    user = User.find_by(api_key: params[:api_key])
+    render json: FavoriteSerializer.new(user)
+  end
+  # def index
+  #   user = User.find_by(api_key: params[:api_key])
+  #   binding.pry
+  #   favorites = user.favorites
+  #   favorite = FavoriteFacade.new(user.favorites).fav_current_weather
+  #   render json: FavoriteSerializer.new(favorite)
+  # end
 end
